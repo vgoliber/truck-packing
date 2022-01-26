c8f88df3634c9380ab2830d53e0eb69f5a247141
Victoria Goliber <vgoliber@dwavesys.com>
2022-01-26 13:27:14 +0100

    Suggested changes

--- README.md
+++ README.md
@@ -1,35 +1,60 @@
 # Packing a Delivery Truck
 
 In North America there are hundreds of standard box sizes that consumer goods can be
-shipped in. Selecting and loading these different sized parcels onto delivery trucks 
-makes for a very complex optimization problem, especially when other factors such as 
-priority shipping and order date are considered. 
+shipped in. Selecting and loading these different sized parcels onto delivery trucks
+makes for a very complex optimization problem, especially when other factors such as
+priority shipping and order date are considered.
 
-In this example we consider a simple scenario where we have a single truck and single 
-box size. We want to choose packages to load onto the truck so that the number of 
+In this example we consider a simple scenario where we have a single truck and single
+box size. We want to choose packages to load onto the truck so that the number of
 packages with priority shipping status is maximized and the aggregate transit time is
 minimized. The truck's weight and size capacity must not be exceeded.
 
 ## Usage
+
 To run the example, type:
 
-    python truck_packing_demo.py
+`python truck_packing_demo.py`
 
-The code will randomly generate a problem with 300 available packages with a weight between 
+The code will randomly generate a problem with 300 available packages with a weight between
 1 and 100 lbs. It generates two tables to show the problem and the results.
 
-![readme_images](readme_images/solution.png "solution")
+```bash
+****************** PROBLEM ******************
+
+            Days since order was placed
+Priority |    3    2    1    0
+----------------------------------------
+    3        23   26   20   25
+    2        22   23   31   28
+    1        24   28   24   26
+
+****************** SOLUTION ******************
+
+            Days since order was placed
+Priority |    3    2    1    0
+----------------------------------------
+    3        20   21   11    6
+    2        14    5    5    0
+    1        13    4    1    0
+
+Total number of selected items: 100
+Total weight of selected items: 2995
+```
 
 The upper left quadrant of the table contains the highest priority packages that have been
-in transit for the longest. The solution should show more packages in the first column and 
-row than the others. 
+in transit for the longest. The solution should show more packages in the first column and
+row than the others.
 
 ## Problem Formulation
+
 This is a multi-objective and multi-constraint problem, so we will use the Constrained
-Quadratic Model (CQM) hybrid solver to solve it. 
+Quadratic Model (CQM) hybrid solver to solve it.
 
 ### Problem Parameters
+
 The problem parameters are
+
 - `N`:     the number of packages available
 - `W_max`: the truck's maximum payload
 - `P_max`: the maximum number of packages that can fit in the truck
@@ -37,24 +62,27 @@ The problem parameters are
 - `d_i`:   number of days that package i has been in transit
 
 ### Variables
+
 This problem uses binary variables to indicate whether or not a package is selected.
 
 `x_i` determines if package i is selected (`x_i = 1`) or not (`x_i = 0`)
 
 ### Objective
+
 1. Maximize the number of packages with priority shipping status:
 
-![readme_images](readme_images/objective_priority.png "obj_priority")
+    ![readme_images](readme_images/objective_priority.png "obj_priority")
 
 2. Minimize the total transit time across all packages:
 
-![readme_images](readme_images/objective_transit.png "obj_transit")
+    ![readme_images](readme_images/objective_transit.png "obj_transit")
 
 ### Constraints
+
 1. The total weight of the packages cannot exceed the maximium weight capacity of the truck:
 
-![readme_images](readme_images/constraint_weight.png "constraint_weight")
+    ![readme_images](readme_images/constraint_weight.png "constraint_weight")
 
 2. The total number of packages cannot exceed that which can fit on the truck:
 
-![readme_images](readme_images/constraint_size.png "constraint_size")
+    ![readme_images](readme_images/constraint_size.png "constraint_size")
--- truck_packing_demo.py
+++ truck_packing_demo.py
@@ -1,114 +1,125 @@
-# Copyright 2021 D-Wave Systems Inc.
+# Copyright 2022 D-Wave Systems Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
 
-from dimod import ConstrainedQuadraticModel, Binary, quicksum
-from dwave.system import LeapHybridCQMSampler
-import random
+from dimod import ConstrainedQuadraticModel, Binaries, quicksum 
+from dwave.system import LeapHybridCQMSampler 
+import random 
 import numpy as np
 
 # Problem set up
 num_packages = 300
 
 # Priority of each package, 3 = high priority, 1 = low priority
-priority = [random.choice([1,2,3]) for i in range(num_packages)]
+priority = random.choices((1, 2, 3), k=num_packages)
 
-# Number of days since each package was ordered (More days need to be prioritized higher)
-days_since_order = [random.choice([0,1,2,3]) for i in range(num_packages)]
+# Number of days since each package was ordered (More days need to be
+# prioritized higher)
+days_since_order = random.choices((0, 1, 2, 3), k=num_packages)
 
 # Weight of each package
-weight = [random.randint(1, 100) for i in range(num_packages)]
+weight = random.choices(range(1, 101), k=num_packages)
 
 # Maximum weight and number of packages the truck can handle
 max_weight = 3000  
 max_packages = 100
 
 # Weights for the objective functions
-obj_weight_priority = 1.0
+obj_weight_priority = 1.0 
 obj_weight_days = 1.0
 
 # Build the CQM
 cqm = ConstrainedQuadraticModel()
 
 # Create the binary variables
-bin_variables = [Binary(i) for i in range(num_packages)]
+bin_variables = list(Binaries(range(num_packages)))
 
-# ----------------- Objective functions -----------------
+# ----------------- Objective functions ----------------- 
+
 # 1. Maximize priority shipping packages
-objective1 = -obj_weight_priority * quicksum(priority[i] * bin_variables[i] for i in range(num_packages))
+objective1 = -obj_weight_priority * quicksum(priority[i] * bin_variables[i] 
+                for i in range(num_packages))
 
 # 2. Minimize customers wait time
-objective2 = -obj_weight_days * quicksum(days_since_order[i] * bin_variables[i] for i in range(num_packages))
+objective2 = -obj_weight_days * quicksum(days_since_order[i] * bin_variables[i]
+                for i in range(num_packages))
 
 # Add the objectives to the CQM
 cqm.set_objective(objective1 + objective2)
 
-# ----------------- Constraints -----------------
+# ----------------- Constraints ----------------- 
+
 # Add the maximum capacity constraint
-cqm.add_constraint(quicksum(weight[i] * bin_variables[i] for i in range(num_packages)) <= max_weight,
-                   label='max_weight')
+cqm.add_constraint(quicksum(weight[i] * bin_variables[i] for i in
+range(num_packages)) <= max_weight, label='max_weight')
 
 # Add the maximum package (or truck size) constraint
-cqm.add_constraint(quicksum(bin_variables[i] for i in range(num_packages)) == max_packages,
-                   label='max_packages')
+cqm.add_constraint(quicksum(bin_variables[i] for i in range(num_packages)) 
+                    == max_packages, label='max_packages')
 
 # -----------------  Submit to the CQM sampler -----------------
-cqm_sampler = LeapHybridCQMSampler()
+cqm_sampler = LeapHybridCQMSampler() 
 sampleset = cqm_sampler.sample_cqm(cqm, label='Truck Packing Demo')
 
 # ----------------- Process the results -----------------
-feasible_sols = np.where(sampleset.record.is_feasible == True)
+feasible_sampleset = sampleset.filter(lambda d: d.is_feasible)
 
-if len(feasible_sols[0]):
-    first_feasible_sol = np.where(sampleset.record[feasible_sols[0][0]][0] == 1)
+if not len(feasible_sampleset): 
+    print("\nNo feasible solution found.\n")
+
+else: 
+    first_feasible_sol = feasible_sampleset.first.sample
 
     # Characterize the problem
-    problem_array = np.zeros((3, 4)).astype(int)
+    problem_array = np.zeros((3, 4)).astype(int) 
     for i in range(num_packages):
         problem_array[-1 * (priority[i]-3)][-1 * (days_since_order[i] - 3)] += 1
 
-    print("\n****************** PROBLEM ******************\n")
-    print('            Days since order was placed')
-    print('{:>5s}{:>5s}{:>5s}{:>5s}{:>5s}'.format('Priority |', '3', '2', '1', '0'))
+    print("\n****************** PROBLEM ******************\n") 
+    print('Days since order was placed')
+    print('{:>5s}{:>5s}{:>5s}{:>5s}{:>5s}'.format('Priority |',
+             '3', '2', '1', '0')) 
     print('-' * 40)
 
-    for i in range(3):
-        print('{:>5s}{:>10s}{:>5s}{:>5s}{:>5s}'.format(str(-1*(i - 3)), str(problem_array[i][0]), str(problem_array[i][1]),
-                                                       str(problem_array[i][2]), str(problem_array[i][3])))
+    for i in range(3): 
+        print('{:>5s}{:>10s}{:>5s}{:>5s}{:>5s}'.format(str(-1*(i- 3)), 
+                str(problem_array[i][0]), str(problem_array[i][1]),
+                str(problem_array[i][2]), str(problem_array[i][3])))
 
-    # Calculate number of packages with each priority and number of days since order in the solution
-    total_weight = quicksum(weight[i] for i in first_feasible_sol[0])
-    solution_priorities = [priority[i] for i in first_feasible_sol[0]]
-    solution_days_since_order = [days_since_order[i] for i in first_feasible_sol[0]]
+    # Calculate number of packages with each priority and number of days since
+    # order in the solution
+    chosen = [i for i in first_feasible_sol if first_feasible_sol[i] == 1]
+    total_weight = quicksum(weight[i] for i in chosen) 
+    solution_priorities = [priority[i] for i in chosen] 
+    solution_days_since_order = [days_since_order[i] for i in chosen]
 
-    # Characterize the solution
-    #   Packages with higher priority (upper row) and the most days since the order (left most column) should be
-    #   prioritized in the selection
-    results_array = np.zeros((3, 4)).astype(int)
-    for i in first_feasible_sol[0]:
+    # Characterize the solution 
+    # Packages with higher priority (upper row) and the most days since the
+    # order (left most column) should be prioritized in the selection
+    results_array = np.zeros((3, 4)).astype(int) 
+    for i in chosen:
         results_array[-1 * (priority[i]-3)][-1 * (days_since_order[i] - 3)] += 1
 
-    print("\n****************** SOLUTION ******************\n")
-    print('            Days since order was placed')
-    print('{:>5s}{:>5s}{:>5s}{:>5s}{:>5s}'.format('Priority |', '3', '2', '1', '0'))
+    print("\n****************** SOLUTION ******************\n") 
+    print('Days since order was placed')
+    print('{:>5s}{:>5s}{:>5s}{:>5s}{:>5s}'.format('Priority |',
+            '3', '2', '1', '0')) 
     print('-' * 40)
 
-    for i in range(3):
-        print('{:>5s}{:>10s}{:>5s}{:>5s}{:>5s}'.format(str(-1*(i - 3)), str(results_array[i][0]), str(results_array[i][1]),
-                                                       str(results_array[i][2]), str(results_array[i][3])))
+    for i in range(3): 
+        print('{:>5s}{:>10s}{:>5s}{:>5s}{:>5s}'.format(str(-1*(i - 3)),
+                str(results_array[i][0]), str(results_array[i][1]),
+                str(results_array[i][2]), str(results_array[i][3])))
 
-    print(("\nTotal number of selected items: {}".format(len(first_feasible_sol[0]))))
+    print(("\nTotal number of selected items: {}".format(len(chosen))))
     print("Total weight of selected items: {}".format(total_weight))
-else:
-    print("No feasible solutions found")
-
